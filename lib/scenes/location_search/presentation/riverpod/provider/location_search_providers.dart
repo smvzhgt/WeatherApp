08186c0ac700ebc0927e567987fe21e41117c1f0
Oleg <smvzhgt@gmail.com>
2021-10-26 00:28:01 +0300

    add providers for interactors

--- lib/scenes/location_search/presentation/riverpod/provider/location_search_providers.dart
+++ lib/scenes/location_search/presentation/riverpod/provider/location_search_providers.dart
@@ -1,11 +1,20 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../domain/data_providers/location_search_data_provider.dart';
 
 import '../../../../../core/state.dart';
 import '../../../../../di/injection.dart';
 import '../../../domain/interactors/location_search_interactor_impl.dart';
 import '../notifier/location_search_notifier.dart';
 
+final locationSearchInteractorProvider = Provider<LocationSearchInteractor>(
+  (ref) => LocationSearchInteractorImpl(
+    dataProvider: sl<LocationSearchDataProvider>(),
+  ),
+);
+
 final locationSearchNotifierProvider =
-    StateNotifierProvider<LocationSearchNotifier, WeatherState>((ref) {
-  return LocationSearchNotifier(sl<LocationSearchInteractor>());
-});
+    StateNotifierProvider<LocationSearchNotifier, WeatherState>(
+  (ref) => LocationSearchNotifier(
+    ref.watch(locationSearchInteractorProvider),
+  ),
+);
