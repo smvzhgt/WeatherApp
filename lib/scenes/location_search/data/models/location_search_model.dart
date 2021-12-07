e43c6650d2b1c6d670e96b5ab816985949cfa4fb
Oleg <smvzhgt@gmail.com>
2021-12-07 13:03:02 +0200

    add changes to model and entity to save to db

--- lib/scenes/location_search/data/models/location_search_model.dart
+++ lib/scenes/location_search/data/models/location_search_model.dart
@@ -6,12 +6,46 @@ part 'location_search_model.g.dart';
 @JsonSerializable(fieldRename: FieldRename.snake)
 class LocationSearchModel extends LocationSearch {
   const LocationSearchModel({
+    int? id,
     required String title,
     required String locationType,
     required int woeid,
-  }) : super(title: title, locationType: locationType, woeid: woeid);
+  }) : super(
+          id: id,
+          title: title,
+          locationType: locationType,
+          woeid: woeid,
+        );
 
   factory LocationSearchModel.fromJson(Map<String, dynamic> json) =>
       _$LocationSearchModelFromJson(json);
   Map<String, dynamic> toJson() => _$LocationSearchModelToJson(this);
+
+  LocationSearchModel copy({
+    int? id,
+    String? title,
+    String? locationType,
+    int? woeid,
+  }) =>
+      LocationSearchModel(
+          id: id ?? this.id,
+          title: title ?? this.title,
+          locationType: locationType ?? this.locationType,
+          woeid: woeid ?? this.woeid);
+}
+
+// Data base
+class LocationSearchFields {
+  static const String tableName = "locationSearch";
+  static const List<String> values = [
+    id,
+    title,
+    locationType,
+    woeid,
+  ];
+
+  static const String id = "_id";
+  static const String title = "title";
+  static const String locationType = "locationType";
+  static const String woeid = "woeid";
 }
--- lib/scenes/location_search/domain/entities/location_search_entity.dart
+++ lib/scenes/location_search/domain/entities/location_search_entity.dart
@@ -1,16 +1,18 @@
 import 'package:equatable/equatable.dart';
 
 class LocationSearch extends Equatable {
+  final int? id;
   final String title;
   final String locationType;
   final int woeid;
 
   const LocationSearch({
+    this.id,
     required this.title,
     required this.locationType,
     required this.woeid,
   });
 
   @override
-  List<Object?> get props => [title, locationType, woeid];
+  List<Object?> get props => [id, title, locationType, woeid];
 }
